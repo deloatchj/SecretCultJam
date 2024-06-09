c6967d0d57bf01725f0786a9ca81d2e8873d85a2
deloatchj <113311736+deloatchj@users.noreply.github.com>
2024-06-08 23:41:53 -0400

    Cell.gd

--- scripts/Cell.gd
+++ scripts/Cell.gd
@@ -5,7 +5,7 @@ signal mine_clicked
 
 var is_mine = false
 var mine_count = 0
-var is_flagged = false
+var is_flagged = false  # Add a flag state
 
 var texture_bomb
 var texture_tile0
@@ -14,7 +14,7 @@ var texture_tile2
 var texture_tile3
 var texture_tile4
 var texture_tile5
-var texture_flag
+var texture_flag  # Add a variable for the flag texture
 
 @onready var texture_rect = $TextureRect
 
@@ -26,7 +26,7 @@ func _ready():
 	texture_tile3 = preload("res://art/Minesweeper/tile3.png")
 	texture_tile4 = preload("res://art/Minesweeper/tile4.png")
 	texture_tile5 = preload("res://art/Minesweeper/tile5.png")
-	texture_flag = preload("res://art/Minesweeper/tileflag.png")
+	texture_flag = preload("res://art/Minesweeper/tileflag.png")  # Preload the flag texture
 
 	connect("pressed", Callable(self, "_on_pressed"))
 	_update_texture()
@@ -76,12 +76,24 @@ func _update_texture():
 		texture_rect.texture = texture_tile0
 
 func _gui_input(event):
-	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_RIGHT and event.pressed:
-		if is_flagged:
-			texture_rect.texture = texture_tile0
-			is_flagged = false
-		else:
-			# Place the flag
-			texture_rect.texture = texture_flag
-			is_flagged = true
-		return
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_RIGHT && event.pressed:
+			if is_flagged:
+				# Remove the flag if already flagged
+				texture_rect.texture = texture_tile0
+				is_flagged = false
+			else:
+				# Place the flag
+				texture_rect.texture = texture_flag
+				is_flagged = true
+			return
+
+func _input(event):
+	if event is InputEventKey && event.pressed:
+		if event.keycode == KEY_R:
+			reset_game()
+
+func reset_game():
+	print("Resetting the game...")
+	get_tree().change_scene_to_file("res://scenes/MainMenu.tscn")
+
