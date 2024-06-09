51ed9b2c3ac4bcf7b0e9a5dce3dd08ccf1cab47a
ArceusMaxis <octohands@gmail.com>
2024-06-09 11:30:22 +0530

    amirtha branch merge- credits basic + new final extended fail anim

--- scripts/GameManager.gd
+++ scripts/GameManager.gd
@@ -1,14 +1,23 @@
 extends Node
 
-var losestate :int = 0
+var losestate : int = 0
 var evil : bool = false
+var recentflag : bool = false
+var minesweeperwincounter : int
+var winstate : bool = false
+# TODO: increment losestate whenever player loses
+# TODO: Make sure its impossible for state to cross 6
 
-# TODO increment losestate whenever player loses
-# TODO Make sure its impossible for state to cross 6
-
-# TODO use 'evil' to determine main menu UI
+# TODO: use 'evil' to determine main menu UI
 func _ready():
 	pass # Replace with function body.
 
 func _process(delta):
-	pass
+	if losestate > 0:
+		evil = true
+	
+	if minesweeperwincounter >= 5:
+		winstate = true
+
+	if winstate == true:
+		get_tree().change_scene_to_file("res://scenes/MainMenu.tscn")
--- scripts/postgame.gd
+++ scripts/postgame.gd
@@ -1,11 +1,7 @@
 extends Control
 
-var full : bool
-var one : bool
-var two : bool
-var three : bool
-var four : bool
-var five : bool
+@onready var losestate = GameManager.losestate
+@onready var recentlose = GameManager.recentflag
 
 func _physics_process(_delta):
 	if Input.is_action_just_pressed("one"):
@@ -19,4 +15,43 @@ func _physics_process(_delta):
 	if Input.is_action_just_pressed("five"):
 		$AnimationPlayer.play("fourtofive")
 	if Input.is_action_just_pressed("fail"):
+		$AnimationPlayer.play("final_fail")
+
+func _ready():
+	var evil_cursor = load("res://cursors/evil_cursor.svg")
+	Input.set_custom_mouse_cursor(evil_cursor)
+	
+	if losestate == 0:
+		$AnimationPlayer.play("preserve_full")
+	if losestate == 1:
+		if recentlose == true:
+			$AnimationPlayer.play("fulltoone")
+		if recentlose == false:
+			$AnimationPlayer.play("preserve_one")
+			
+	if losestate == 2:
+		if recentlose == true:
+			$AnimationPlayer.play("onetotwo")
+		if recentlose == false:
+			$AnimationPlayer.play("preserve_two")
+			
+	if losestate == 3:
+		if recentlose == true:
+			$AnimationPlayer.play("twotothree")
+		if recentlose == false:
+			$AnimationPlayer.play("preserve_three")
+			
+	if losestate == 4:
+		if recentlose == true:
+			$AnimationPlayer.play("threetofour")
+		if recentlose == false:
+			$AnimationPlayer.play("preserve_four")
+			
+	if losestate == 5:
+		if recentlose == true:
+			$AnimationPlayer.play("fourtofive")
+		if recentlose == false:
+			$AnimationPlayer.play("preserve_five")
+	
+	if losestate == 6:
 		$AnimationPlayer.play("final_fail")
