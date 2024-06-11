30c2a337dccc4811f5479cd83fde56eb110bbce1
ArceusMaxis <octohands@gmail.com>
2024-06-11 11:12:23 +0530

    added a small refactor for evil state management

--- scripts/postgame.gd
+++ scripts/postgame.gd
@@ -3,20 +3,6 @@ extends Control
 var losestate = GameManager.minesweeperlosecounter
 var recentlose = GameManager.recentflag
 
-#func _physics_process(_delta):
-	#if Input.is_action_just_pressed("one"):
-		#$AnimationPlayer.play("fulltoone")
-	#if Input.is_action_just_pressed("two"):
-		#$AnimationPlayer.play("onetotwo")
-	#if Input.is_action_just_pressed("three"):
-		#$AnimationPlayer.play("twotothree")
-	#if Input.is_action_just_pressed("four"):
-		#$AnimationPlayer.play("threetofour")
-	#if Input.is_action_just_pressed("five"):
-		#$AnimationPlayer.play("fourtofive")
-	#if Input.is_action_just_pressed("fail"):
-		#$AnimationPlayer.play("final_fail")
-
 func _physics_process(_delta):
 	if Input.is_action_just_pressed("one"):
 		$AnimationPlayer.play("fulltoone")
@@ -37,42 +23,53 @@ func _physics_process(_delta):
 func _ready():
 	var evil_cursor = load("res://cursors/evil_cursor.svg")
 	Input.set_custom_mouse_cursor(evil_cursor)
-	
 	if losestate == 0:
 		$AnimationPlayer.play("preserve_full")
+		GameManager.evil = false
+
 	if losestate == 1:
 		if recentlose == true:
 			$AnimationPlayer.play("fulltoone")
+			GameManager.evil = true
 		if recentlose == false:
 			$AnimationPlayer.play("preserve_one")
+			GameManager.evil = false
 			
 	if losestate == 2:
 		if recentlose == true:
 			$AnimationPlayer.play("onetotwo")
+			GameManager.evil = true
 		if recentlose == false:
 			$AnimationPlayer.play("preserve_two")
+			GameManager.evil = false
 			
 	if losestate == 3:
 		if recentlose == true:
 			$AnimationPlayer.play("twotothree")
+			GameManager.evil = true
 		if recentlose == false:
 			$AnimationPlayer.play("preserve_three")
+			GameManager.evil = false
 			
 	if losestate == 4:
 		if recentlose == true:
 			$AnimationPlayer.play("threetofour")
+			GameManager.evil = true
 		if recentlose == false:
 			$AnimationPlayer.play("preserve_four")
+			GameManager.evil = false
 			
 	if losestate == 5:
 		if recentlose == true:
 			$AnimationPlayer.play("fourtofive")
+			GameManager.evil = true
 		if recentlose == false:
 			$AnimationPlayer.play("preserve_five")
-	
+			GameManager.evil = false
+			
 	if losestate == 6:
 		$AnimationPlayer.play("final_fail")
-		
+		GameManager.evil = true
 
 func _on_button_pressed():
 		GameManager.recentflag = false
